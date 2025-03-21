e7d2f66c22709d455274435ad2fef6ec42c214d7
nikhilbleedsblue <nikhilvarma140@gmail.com>
2025-03-21 15:14:58 +0530

    changed image

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import "./card.css"
 import PhoneIcon from "@mui/icons-material/Phone";
 import { IoMdMail } from "react-icons/io";
@@ -6,14 +6,16 @@ import { ImProfile } from "react-icons/im";
 
 
 import image from "./../assets/image.jpeg"
-import profile1 from "./../assets/profile2.jpeg"
+import profile1 from "./../assets/profile3.jpeg"
+import profile from "./../assets/profile3_color.jpeg"
 const Card = ()=>{
+    const [color,setColor] = useState(false)
     const resumeLink  = '12lYXkgFWUJvCzeUkcaKerNAFwFlBTnWb';
     const driveLink = `https://drive.google.com/uc?export=download&id=${resumeLink}`;
     return(<>
     <div className="card">
-        <div className="img_wrapper">
-            <img src={profile1} alt="alt" />
+        <div className="img_wrapper" onMouseEnter={()=> setColor(true)} onMouseOut={()=>setColor(false) }>
+            <img src= { color? profile:profile1} alt="alt" />
         </div>
         <div className="text_desc">
             I am a front-end developer. I worked as ReactJS developer and I have relevant skills like SASS, NextJS, Git,GitLab,Axios, Material-UI, TypeScript and NodeJS
